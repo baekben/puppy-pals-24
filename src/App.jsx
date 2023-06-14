99dd836d094a87ced03a695ae9efcc6ebc336eb3
Ben Baek <ben10baek@hotmail.com>
2023-06-13 21:12:31 -0700

    Added additional styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,48 @@
 import "./App.css";
-import { puppyList } from "./data";
+import { puppyList } from "./data.js";
 import { useState } from "react";
-
+import "./index.css";
+import "./style.css";
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
   //console.log("puppyList: ", puppyList);
 
   const featuredPup = puppies.find((pup) => pup.id === featPupId);
-  console.log(featPupId);
+  //console.log(featPupId);
   return (
     <>
-      <div>
-        {puppies.map((puppy) => {
-          return (
-            <p
-              onClick={() => {
-                setFeatPupId(puppy.id);
-              }}
-              key={puppy.id}
-            >
-              {puppy.name}
-            </p>
-          );
-        })}
-        <p>
-          {featPupId && (
-            <div>
-              <h2>{featuredPup.name}</h2>
-              <ul>
-                <li>Age: {featuredPup.age}</li>
-                <li>Email:{featuredPup.email}</li>
-              </ul>
-            </div>
-          )}
-        </p>
+      <h1>Puppies</h1>
+      <div className="App">
+        <div className="puppies">
+          Select a puppy to learn more about them
+          {puppies.map((puppy) => {
+            return (
+              <p
+                className="puppy"
+                onClick={() => {
+                  setFeatPupId(puppy.id);
+                }}
+                key={puppy.id}
+              >
+                {puppy.name}
+              </p>
+            );
+          })}
+        </div>
+
+        {featPupId && (
+          <div className="puppyInfo">
+            <h2>
+              <u>More Info</u>
+              <br></br>Puppy: {featuredPup.name}
+            </h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email:{featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
       </div>
     </>
   );
