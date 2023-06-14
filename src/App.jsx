9948e016e46e0bb504eeedc05d63efc301607baf
Ben Baek <ben10baek@hotmail.com>
2023-06-13 20:13:31 -0700

    added puppies data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import './App.css'
+import "./App.css";
+import { puppyList } from "./data";
+import { useState } from "react";
 
 function App() {
 
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log("puppyList: ", puppyList);
   return (
     <>
-      <div>
-        
-      </div>
+      <div>{puppies.map((puppy)=>{return <p key={puppy.id}>{puppy.name}</p>})}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
