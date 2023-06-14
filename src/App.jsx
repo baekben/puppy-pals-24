a2ae0790c1547cec0f751c1a1feb2bc6844afa10
Ben Baek <ben10baek@hotmail.com>
2023-06-13 20:37:20 -0700

    added puppy name, age, email display

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,39 @@ import { puppyList } from "./data";
 import { useState } from "react";
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  //console.log("puppyList: ", puppyList);
 
-  const [puppies, setPuppies] = useState(puppyList)
-  console.log("puppyList: ", puppyList);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featPupId);
   return (
     <>
-      <div>{puppies.map((puppy)=>{return <p key={puppy.id}>{puppy.name}</p>})}</div>
+      <div>
+        {puppies.map((puppy) => {
+          return (
+            <p
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
+        })}
+        <p>
+          {featPupId && (
+            <div>
+              <h2>{featuredPup.name}</h2>
+              <ul>
+                <li>Age: {featuredPup.age}</li>
+                <li>Email:{featuredPup.email}</li>
+              </ul>
+            </div>
+          )}
+        </p>
+      </div>
     </>
   );
 }
